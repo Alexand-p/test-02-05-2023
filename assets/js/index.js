a9807042dfdcdbf967a0ce77a2212ad2c080e047
Alexand-p <3082303@gmail.com>
2023-05-05 18:35:34 +0300

    add slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,17 @@ const clearInput = () => {
   const clearBtn = document.getElementById("clear-btn");
   clearBtn.addEventListener("click", clearInput);
   
- 
+// Slider 
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
